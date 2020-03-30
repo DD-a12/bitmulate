72002bf9c19ae2181d31dd9cd3eb4166b618f398
DD-a12 <57923229+DD-a12@users.noreply.github.com>
2020-03-30 01:29:15 -0700

    a

---
+++ bitcoinmulate-backend/README.md
@@ -0,0 +1,10 @@
+# bitmulate-backend
+
+## Stack
+- Node.js
+ - Koa
+ - Mongoose
+ - Websocket
+- MongoDB
+
+> More information wil be documented in the future
---
+++ bitcoinmulate-backend/src/db/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const {
+    MONGO_URI: mongoURI
+} = process.env;
+
+module.exports = (function () {
+
+    return {
+        connect () { 
+            mongoose.connect(mongoURI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }).then(
+                () => {
+                    console.log('Successfully connected to mongodb')
+                }
+            ).catch(e => {
+                console.error(e);
+            });
+        }
+    }
+})();
