c54cb7a11d94b99b56f2138f680df20effdc192d
DD-a12 <57923229+DD-a12@users.noreply.github.com>
2020-04-03 23:12:31 -0700

    b

---
+++ bitcoinmulate-frontend/src/components/templates/PageTemplate/PageTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './PageTemplate.scss'
+
+const cx = classNames.bind(styles);
+
+const PageTemplate = ({header, children}) => {
+    <div classNames={cx('page')}>
+        <header>
+            {header}
+        </header>
+        <div classNames={cx('content', {
+            'has-header': header // if there is a header, gives 3.5 padding to top
+        })}>
+            {children}
+        </div>
+    </div>
+}
+
+export default PageTemplate;
